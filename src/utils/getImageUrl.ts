2f8381c3a58e3cf66e33fdb78ca76e0cc8dfdf45
briantimothykarani <timothykaranibrian@gmail.com>
2025-10-22 15:19:35 +0300

    Updates

--- src/utils/getImageUrl.ts
+++
@@ -1,5 +0,0 @@
-export function getImageUrl(path?: string | null): string {
-    if (!path) return "/default-avatar.png";
-    if (path.startsWith("http")) return path;
-    return `http://127.0.0.1:8000${path}`;
-}
